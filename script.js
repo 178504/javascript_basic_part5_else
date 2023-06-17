17e5689da153fa6294897afbb0e2c102a90bb182
tsujishouta <koshi51455@gmail.com>
2023-06-17 17:58:03 +0900

    second

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //1. elseを使用する時の注意点
-//elseを使用するときは指定した値がtrueなのかfalseなのかどちらの処理で行うか処理をしなくてはならないので考えながら処理を行う必要がある。
+//elseを使用するときは指定した値がtrueなのかfalseなのかどちらの処理で行うか処理をしなくてはならないので考えながら使用する必要がある。
 
 //2. JavaScriptで実装した時の確認ポイント
 //実装する際はデベロッパーツールでエラーが発生してないかちゃんと表示されているか確認しながら実装する。
@@ -9,18 +9,17 @@ var variableNumber = 178;
 if(variableNumber){
   console.log("trueの処理");
   console.log(variableNumber);
-}
-else{
+}else{
   console.log("falseの処理");
   console.log(variableNumber);
-}
+};
 //4. 文字列型を比較してelseの処理に入る処理を作ってください。
 var variableText = "text";
 if(variableText){
   console.log("trueの処理");
   console.log(variableText);
-}
-else{
+}else{
   console.log("falseの処理");
   console.log(variableText);
-}
+};
+
